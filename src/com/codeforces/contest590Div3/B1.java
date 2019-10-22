b6e15a8b6491c9c1e6291c2b8517de68a156a3c9
Abbes Yassine <abbesyassine4@gmail.com>
2019-10-22 16:39:56 +0200

    adding prb B1 590 Div 3

--- src/com/codeforces/contest590Div3/B1.java
+++ src/com/codeforces/contest590Div3/B1.java
@@ -1,5 +1,7 @@
 package com.codeforces.contest590Div3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class B1 {
@@ -7,7 +9,26 @@ public class B1 {
         // write your code here
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
+        int[] array = new int[n];
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+
+            int index = res.indexOf(array[i]);
+            if (index == -1) {
+                res.add(0, array[i]);
+                if (res.size() > k) {
+                    res.remove(res.size() - 1);
+                }
+            }
+        }
+        System.out.println(res.size());
+        for (int i = 0; i < res.size(); i++) {
+            System.out.print(res.get(i) + " ");
+        }
     }
 
 
